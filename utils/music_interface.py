d7a5a26bbdf8c6f9744b0cc98b61b132aa6c19ac
Ty Chermsirivatana <professornitro@gmail.com>
2021-09-28 20:18:50 -0400

    Silence console messages

--- utils/music_interface.py
+++ utils/music_interface.py
@@ -2,11 +2,19 @@ import youtube_dl
 
 from utils import default
 
+# Silence console warning errors 
+youtube_dl.utils.bug_reports_message = lambda: ''
+
+
 ytdl = youtube_dl.YoutubeDL(
     default.get(file='ytdl_format.json', named_tuple=False)
 )
 
 ffmpeg_opts = default.get(file='ytdl_format.json', named_tuple=False)
+
+
+
+
 
 """ Returns a Discord audio player from a Youtube link. """
 def from_url_yt(url):
