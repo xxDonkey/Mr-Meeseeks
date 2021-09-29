dcaed62d76e4450a94101e0f9a3fbd91201b9c75
Dylan R <d.raiff@icloud.com>
2021-09-28 20:47:22 -0400

    Update data.py

--- utils/data.py
+++ utils/data.py
@@ -4,6 +4,7 @@ import random
 from utils import permissions
 from utils import default
 from discord.ext.commands import Bot
+from utils import music_interface
 
 class Bot(Bot):
     def __init__(self, *args, **kwargs):
@@ -26,15 +27,44 @@ class Embed(discord.Embed):
 class Queue():
     def __init__(self):
         self.q = []
+        self.ctx = None
 
     def __str__(self):
         return self.q.__str__()
 
-    def add(self, audio_player):
-        self.q.append(audio_player)
+    async def play_next(self, ctx):
+        if self.q == []:
+            # queue is empty
+            return
 
-    def remove(self):
-        pass
+        ctx.voice_client.play(self.q[0], after=after_play)
+
+    async def after_play(self, e):
+        self.remove()
+
+
+    async def add(self, to_add, index=0):
+        to_play = to_play.lower()
+        audio_source = None
+
+        # check if its a link -- rudimentary test here, get smth better
+        if to_play.startswith('https://'):
+            # check if its youtube
+            if to_play.contains('youtube'):
+                audio_source = await music_interface.from_url_yt(to_play)
+
+            # check if its spotify
+            elif to_play.contains('spotify'):
+                audio_source = await music_interface.from_url_spotify()(to_play)
+
+        # if not a link, search youtube
+        else:
+            audio_source = await music_interface.from_search(to_play)
+
+        self.q.insert(audio_source, index)
+
+    def remove(self, index=0):
+        self.q.pop(index)
 
     def shuffle(self):
         random.shuffle(self.q)
