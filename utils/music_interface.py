79952194636f1ee1ad707bf4e4e97cc292caacd2
Dylan R <d.raiff@icloud.com>
2021-10-04 09:34:35 -0400

    Update music_interface.py

--- utils/music_interface.py
+++ utils/music_interface.py
@@ -17,7 +17,11 @@ ffmpeg_opts = default.get(file='ffmpeg_opts.json', named_tuple=False)
 """ Returns a Discord audio player from a Youtube link. """
 async def from_url_yt(url, loop=None):
     loop = loop or asyncio.get_event_loop()
-    data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=False))
+
+    try:
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=False))
+    except youtube_dl.utils.DownloadError:
+        return None
 
     # if its a playlist, take first entry
     if 'entries' in data:
@@ -25,7 +29,7 @@ async def from_url_yt(url, loop=None):
 
     filename = ytdl.prepare_filename(data)
     return discord.PCMVolumeTransformer(
-        discord.FFmpegPCMAudio(filename **ffmpeg_opts),
+        discord.FFmpegPCMAudio(filename, **ffmpeg_opts),
         data=data
     )
     
@@ -37,10 +41,11 @@ async def from_url_spotify(url, loop=None):
 """ Returns a Discord audio player from a Youtube keyword. """
 async def from_search(search, loop=None):
     # search youtube for the URL
-    url_result = YoutubeSearch(search, max_results=1).to_dict()[0]
+    data = YoutubeSearch(search, max_results=1).to_dict()[0]
+    url = 'https://www.youtube.com' + data['url_suffix']
 
     # get the link's source
-    return from_url_yt(url_result)
+    return await from_url_yt(url)
 
     
     
