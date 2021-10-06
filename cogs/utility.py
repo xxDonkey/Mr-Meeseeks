bc36071861e461311a4b15b51cd35547879ced72
Dylan R <d.raiff@icloud.com>
2021-10-06 19:53:24 -0400

    added queue command

--- cogs/utility.py
+++ cogs/utility.py
@@ -3,6 +3,7 @@ import math
 
 from discord.ext import commands
 from utils import default
+from utils.data import Embed
 
 class Utility(commands.Cog):
     def __init__(self, bot):
@@ -17,6 +18,25 @@ class Utility(commands.Cog):
         user = '{0.user}'.format(self.bot)
         response = f'{user} has a response time of {math.round(latency)} ' + (':exclamation: ' * level)
         await ctx.channel.send(response)
+
+    @commands.command(aliases=['q'])
+    async def queue(self, ctx):
+        q = self.bot.q.get_q()
+        
+        embed = Embed(
+            bot=self.bot,
+            title=f'Queue for {ctx.message.guild}'
+        )
+
+        for i, song in enumerate(q):
+            embed.add_field(
+                name=f'#{i + 1}. {song[0]} - {song[1]}', 
+                value='\u200b',
+                inline=False
+            )
+
+        await ctx.channel.send(embed=embed)
+
 
 def setup(bot):
     bot.add_cog(Utility(bot))
