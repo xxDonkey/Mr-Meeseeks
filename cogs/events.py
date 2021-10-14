30b7ea2123d5e56ec6d064b29e2baa9b38e634fa
Dylan R <d.raiff@icloud.com>
2021-10-13 20:13:35 -0400

    removed fucked merging

--- cogs/events.py
+++ cogs/events.py
@@ -29,10 +29,6 @@ class Events(commands.Cog):
 
     """ Called when an error is encountered when handling a command request. """
     @commands.Cog.listener()
-    async def on_voice_state_update(self, member, before, after):
-        print(member, self.bot.user.__str__())
-
-    @commands.Cog.listener()
     async def on_command_error(self, ctx, err):
         if isinstance(err, errors.MissingRequiredArgument) or isinstance(err, errors.BadArgument) or isinstance(err, errors.TooManyArguments):
             cmd = str(ctx.invoked_subcommand) if ctx.invoked_subcommand else str(ctx.command)
